10f5a61b53e9f2a36d3c686003ecd5d12f841d10
sink-opuba <39055986+sink-opuba@users.noreply.github.com>
2020-05-02 07:54:09 +0100

    fix state data issue

--- helpers/index.js
+++ helpers/index.js
@@ -17,24 +17,21 @@ exports.parseStatesSummary = (statesSummary) => {
   let stateData = [...statesSummary];
   stateData.shift();
   stateData.pop();
-  stateData = stateData
-    .map((elem) => {
-      elem = elem
-        //removes newline or comma breaks, space between words, and split into array from more than one space
-        .replace(/\n|\,/g, " ")
-        .replace(/([a-zA-Z])\s([a-zA-Z])/, "$1$2")
-        .trim()
-        .split(/[ ]{1,}/); // =>  ['Bauchi', '8', '5', '0', '0']
-      return {
-        state: elem[0],
-        _id: shortid.generate(), //generates unique ID
-        confirmedCases: +elem[1],
-        casesOnAdmission: +elem[2],
-        discharged: +elem[3],
-        death: +elem[4],
-      };
-    })
-    // remove empty data
-    .filter((data) => data.state !== "");
+  stateData = stateData.map((elem) => {
+    elem = removeComma(elem)
+      //removes newline or comma breaks, space between words, and split into array from more than one space
+      .replace(/\n/g, " ")
+      .replace(/([a-zA-Z])\s([a-zA-Z])/, "$1$2")
+      .trim()
+      .split(/[ ]{1,}/); // =>  ['Bauchi', '8', '5', '0', '0']
+    return {
+      state: elem[0],
+      _id: shortid.generate(), //generates unique ID
+      confirmedCases: +elem[1],
+      casesOnAdmission: +elem[2],
+      discharged: +elem[3],
+      death: +elem[4],
+    };
+  });
   return stateData;
 };
